5a39e5d27495c8d0aa5ff164b8c0e06d2c8a8547
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2024-01-10 22:05:44 +0100

    chg(): Update environment

--- FRONTEND/src/environments/environment.ts
+++ FRONTEND/src/environments/environment.ts
@@ -1,9 +1,5 @@
 export const environment = {
    production: true,
-   // backendLoginClient: 'http://localhost:8080/api/utilisateur/login',
-   // backendCatalogue: 'http://localhost:8080/api/catalogue',
-   // backendCreationCompte: 'http://localhost:8080/api/utilisateur/creer'
-
    backendLoginClient: 'https://projet-siweb-winness.onrender.com/api/utilisateur/login',
    backendCatalogue: 'https://projet-siweb-winness.onrender.com/api/catalogue',
    backendCreationCompte: 'https://projet-siweb-winness.onrender.com/api/utilisateur/creer'
