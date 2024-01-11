89e28f4141f79eccdce8f51e157e8a7ea1067429
Winness Rakotozafy <winness.rakotozafy@outlook.fr>
2024-01-11 06:47:03 +0100

    feat(): Add global error when invalid form

--- FRONTEND/src/app/components/formulaire/formulaire.component.ts
+++ FRONTEND/src/app/components/formulaire/formulaire.component.ts
@@ -9,13 +9,14 @@ import { UtilisateurService } from 'src/services/utilisateur.service';
 })
 export class FormulaireComponent implements OnInit {
   clientForm!: FormGroup;
+  formError!: string;
 
-  regexLogin = /[a-zA-Z0-9.]{1,20}/;
-  regexPassword = /[A-Za-z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\-]{6,20}/;
-  regexTelephone = /[0-9]{10}/;
-  regexCodePostal = /[0-9]{5}/;
-  regexString = /[A-Za-z]{2,30}/;
-  regexEmail = /[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}/;
+  regexLogin = /^[a-zA-Z0-9.]{1,20}$/;
+  regexPassword = /^[A-Za-z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\-]{6,20}$/;
+  regexTelephone = /^[0-9]{10}$/;
+  regexCodePostal = /^[0-9]{5}$/;
+  regexString = /^[A-Za-z]+-?[A-Za-z]+$/;
+  regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 
   constructor(
     private fb: FormBuilder,
@@ -39,14 +40,19 @@ export class FormulaireComponent implements OnInit {
 
   creerUtilisateur() {
     console.log(this.clientForm.value);
-    this.utilisateurService.creerUtilisateur(this.clientForm.value).subscribe(
-      (data) => {
-        console.log('Utilisateur créé avec succès', data);
-      },
-      (error) => {
-        console.error("Erreur lors de la création de l'utilisateur", error);
-      }
-    );
-    this.clientForm.reset();
+    if (this.clientForm.valid) {
+      this.utilisateurService.creerUtilisateur(this.clientForm.value).subscribe(
+        (data) => {
+          console.log('Utilisateur créé avec succès', data);
+        },
+        (error) => {
+          console.error("Erreur lors de la création de l'utilisateur", error);
+        }
+      );
+      this.clientForm.reset();
+    } else {
+      this.formError = 'Le formulaire contient des erreurs. Veuillez vérifier les champs.';
+    }
+    
   }
 }
